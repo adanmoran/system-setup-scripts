6aadb788c9e24b8616c071a421e6d724c5d8e6d9
Adan Moran-MacDonald <adanmoranm@gmail.com>
2019-10-31 16:27:24 -0400

    Readded nvr, since it works for forward search in zathura

--- install_neovim.sh
+++ install_neovim.sh
@@ -30,17 +30,17 @@ function install_neovim() {
 
 }
 
-# This function installs neovim-remote, which allows other
-# programs to use neovim
-#function install_nvr() {
-#	if hash nvr 2>/dev/null; then
-#		echo "NVR already installed"
-#		return 0
-#	fi
-#	pip3 install neovim-remote
-#}
+ This function installs neovim-remote, which allows other
+ programs to use neovim
+function install_nvr() {
+	if hash nvr 2>/dev/null; then
+		echo "NVR already installed"
+		return 0
+   fi
+	pip3 install neovim-remote
+}
 
 install_neovim
-#install_nvr
+install_nvr
 
 # vim: ts=3 sw=3 sts=0 noet :
